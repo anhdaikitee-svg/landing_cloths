3cccd97a32852c0a558455959ae9a9504434907b
Dai Do <doanhdaigr5.2004@gmail.com>
2026-06-27 11:54:12 +0700

    chore: update metadata title to WIN-WIN EMBROIDERY

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ const lora = Lora({
 })
 
 export const metadata: Metadata = {
-  title: "WIN WIN - Bộ Sưu Tập Thời Trang",
+  title: "WIN-WIN EMBROIDERY",
   description: 'Khám phá các bộ sưu tập thời trang cao cấp mang đậm di sản nghệ thuật.',
 }
 
